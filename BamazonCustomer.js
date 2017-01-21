fc11a2a3f88cb2d8b751d2ee423ba4b0d3adc46c
Jeannie Gaines <Jeannie Gaines>
2017-01-21 14:54:36 -0500

    Trying to find a way to update database with the user's input,calcultions

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -40,38 +40,42 @@ var start = function () {
 	   							type:"input",
 	   							message: "How many units would you like?",
 	   							validate: function(value){
-	   								if(isNAN(value) == false){
+	   								if(isNaN(value) == false){
 	   									return true;
 
 	   								} else {
+	   									//console.log(" Please enter a number.")
 	   									return false;
-	   								}
-
+	   								}	
 	   							}
+// The code malfunctions after this line. I cannot get the user's input to match up to the quantity in the database and perform the 
+//appropriate calculations. I want to update the 'StockyQuantity' field by subtracting the 'number' the user inputs from the quantity
+// in the database. Then use this number and multiply the cost to get the total cost. I think I need to do the connection.query first, 
+// before I place the if statement; however, I need the condition before I perform any operations. This is the error I get. My StockQuanity 
+//field is not defined.  ?????
+
+//Which product would you like to select? Shorts
+//? How many units would you like? 7
+//(node:4576) UnhandledPromiseRejectionWarning: Unhandled promise rejection (rejection id: 1): ReferenceError: StockQuantity is not defined
 
 
 	   						}).then (function (answer){
-	   							if (chosenItem.StockQuantity < parseInt(answer.selection)){
-	   								connection.query("UPDATE products SET ? WHERE ? ", [{
-	   									StockQuantity: StockQuantity - answer.choice
+	   							if (chosenItem.StockQuantity > parseInt(answer.selection)){
+	   								connection.query("UPDATE products SET ? WHERE ?", [{
+	   									StockQuantity: StockQuantity - answer.selection
 	   								}], function (err, res){
-	   									console.log("Your purchase has been placed!");
-	   									start ();
+	   									console.log ("Your purchase has been placed. Your total cost is:  " + answer.selection * product.price);
+	   								start();
 	   								});
-	   							} else{
-	   								console.log("Insufficient Quantity!");
+	   					
+	   						} else{
+	   								console.log ("Insufficient quantities left in our inventory.");
 	   								start();
 	   							}
-                              })
-	   					}
-	   				}
-	   			})
+	                        })                                            
+                      }
+	                }
+	            })
 	   		})
+
 	}
-
-	   									
-
-
-
-
-	  
